ffca30adb63ac01c2d0a12e8d7b0ce2add3536ef
Daniil Yarockii <yaroc.d@gmail.com>
2021-06-12 13:13:04 +0300

    fixin train params

--- experiments/sly_six_lemons_mot_dla34.sh
+++ experiments/sly_six_lemons_mot_dla34.sh
@@ -1,3 +1,3 @@
 cd src
-python train.py mot --exp_id sly_six_lemons_mot_dla34 --num_epochs 20 --lr_step '15' --data_cfg '../src/lib/cfg/mot15.json'
+python train.py mot --exp_id mot20_ft_mix_dla34 --num_epochs 20 --lr_step 15 --data_cfg ../src/lib/cfg/sly_mot.json
 cd ..
