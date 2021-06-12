a90634ffd706247749da23f7dd38218727001540
Daniil Yarockii <33942379+Lascarfo@users.noreply.github.com>
2021-06-12 12:35:40 +0300

    Update rename_img_seq.py

--- src/rename_img_seq.py
+++ src/rename_img_seq.py
@@ -1,17 +1,23 @@
 import glob
 import os
 
-paths = sorted(glob.glob('/FairMOT/data/SLY_MOT/images/train/MOT20-01/img1/*.jpg'))
 
-print(paths)
+datasets = os.listdir('/FairMOT/data/SLY_MOT/images/train/')
 
-for i in range(len(paths)):
-    os.rename(paths[i], '{}/{:06d}_temp.jpg'.format('/'.join(paths[i].split('/')[:-1]), i + 1))
+for ds in datasets:
+    
+
+    paths = sorted(glob.glob(f'/FairMOT/data/SLY_MOT/images/train/{ds}/img1/*.jpg'))
+
+    print(paths)
+
+    for i in range(len(paths)):
+        os.rename(paths[i], '{}/{:06d}_temp.jpg'.format('/'.join(paths[i].split('/')[:-1]), i + 1))
 
 
-paths = sorted(glob.glob('/FairMOT/data/SLY_MOT/images/train/MOT20-01/img1/*.jpg'))
+    paths = sorted(glob.glob(f'/FairMOT/data/SLY_MOT/images/train/{ds}/img1/*.jpg'))
 
-print(paths)
+    print(paths)
 
-for i in range(len(paths)):
-    os.rename(paths[i], '{}/{:06d}.jpg'.format('/'.join(paths[i].split('/')[:-1]), i + 1))
+    for i in range(len(paths)):
+        os.rename(paths[i], '{}/{:06d}.jpg'.format('/'.join(paths[i].split('/')[:-1]), i + 1))
